368e10800a56aa68081fe3eb6bd1545a61152829
Vidur Khanal <vidurkhanal@gmail.com>
2024-10-10 14:29:05 -0400

    chore: modularized collectors

--- src/collectors/logger/mod.rs
+++ src/collectors/logger/mod.rs
@@ -1,25 +1,23 @@
-use crate::event::Event;
+mod builder;
+mod tailers;
 
 use super::Collector;
+use crate::event::Event;
 use async_trait::async_trait;
+use tailers::Tailable;
 use tokio::sync::mpsc;
 
 #[derive(Clone)]
-pub struct MetricsCollector {
+pub struct LogsCollector {
     tx: mpsc::Sender<Event>,
-}
-
-impl MetricsCollector {
-    pub fn new(tx: mpsc::Sender<Event>) -> Self {
-        Self { tx }
-    }
+    tailers: Vec<Box<dyn Tailable>>,
 }
 
 #[async_trait]
-impl Collector for MetricsCollector {
+impl Collector for LogsCollector {
     async fn transmit(&mut self) {
         loop {
-            let ev = Event::new_metric("This is a Metric JSON BLOB....");
+            let ev = Event::new_log("This is a Log JSON BLOB....");
             if let Err(_) = self.tx.send(ev).await {
                 println!("receiver dropped");
                 return;
